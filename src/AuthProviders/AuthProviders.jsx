b7da58a5b50a7752ffc4b00c47705e02052bd19d
Rifazul <rifazul50@gmail.com>
2023-10-07 09:50:28 +0600

    add the google in system

--- src/AuthProviders/AuthProviders.jsx
+++ src/AuthProviders/AuthProviders.jsx
@@ -1,9 +1,9 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { createContext, useState } from "react";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null)
-
+const googleProvider = new GoogleAuthProvider()
 const AuthProviders = ({children}) => {
 
 const [user ,setUser] = useState(null)
@@ -13,10 +13,24 @@ const createNewUser = (email,password) =>{
   return createUserWithEmailAndPassword(auth,email,password)
 }
 
+// user SigIn
+const loginUser = (email,password) =>{
+  
+return signInWithEmailAndPassword(auth,email,password)
+}
+
+// Google Login
+const googleLogin = () =>{
+ return signInWithPopup(auth,googleProvider)
+}
+
+
 
 const authInfo = {
  user,
- createNewUser
+ createNewUser,
+ loginUser,
+ googleLogin 
 }
 
 return (
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../AuthProviders/AuthProviders";
+import { toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
 const Login = () => {
+
+const{loginUser,googleLogin } = useContext(AuthContext)
 
 const handlerLogin = (e) =>{
   e.preventDefault()
@@ -8,9 +13,32 @@ const handlerLogin = (e) =>{
   const email = form.get("email")
   const password = form.get("password")
   console.log(email,password);
+
+  // user Login
+  loginUser(email,password)
+  .then(result =>{
+    const userInfo = result.user;
+    toast.success("User Login Successfully")
+  })
+  .catch(error =>{
+    toast.error(error.message)
+  })
 }
 
 
+// Google Login 
+const handlerGoogleLogin = ()=>{
+googleLogin()
+.then(result =>{
+  const userInfo = result.user;
+  toast.success("Google Login Successfully")
+  console.log(userInfo);
+})
+
+.catch(error =>{
+   toast.error(error.message)
+})
+}
 
 return (
 <div>
@@ -36,7 +64,10 @@ return (
       </form>
       
       <p className="mb-4 text-center font-bold text-indigo-400">Do not have an account? Create an account  <Link to="/register" className="text-orange-600"> Register </Link>  </p>
+    
     </div>
+    
+   <div onClick={handlerGoogleLogin } className="text-center w-[400px] mx-auto"> <button className="btn bg-slate-300  text-xl my-3 w-full capitalize"> <FcGoogle/> Google With Login</button></div>
   </div>
  );
 };
